c36c33532498a123abd3dfdcb2ed0d7d08ed17d0
sboukiou <sboukiou@student.1337.ma>
2025-02-27 06:33:57 +0000

    Defining Error notifying macros + Adding includes for deps headers (libft and ft_printf)

--- fdf.h
+++ fdf.h
@@ -2,8 +2,18 @@
 # define FDF_H
 
 # include <mlx.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+# include "dependencies/libft/libft.h"
+# include "dependencies/ft_printf/ft_printf.h"
 
 # define ESCAPE 65307
+# define KEY_PRESS_EVENT 2
+# define KEY_PRESS_MASK 1L<<0
+
+# define MLX_INIT "MLX_INIT"
+# define MLX_WINDOW "MLX_WINDOW"
 
 typedef struct s_data
 {
