4447fe145b690d95dcda31fe8e1798a4e88f0423
sboukiou <sboukiou@student.1337.ma>
2025-02-27 05:28:41 +0000

    Creating a simple event_hook to free all data and exit when pressing escape key

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
-#include <mlx.h>
+#include "fdf.h"
+#include <stdlib.h>
+
+int	handle_key(int key_code, t_data *data)
+{
+	if (key_code == ESCAPE)
+	{
+		mlx_destroy_window(data->mlx, data->mlx_win);
+		mlx_destroy_display(data->mlx);
+		free(data->mlx);
+		exit(0);
+	}
+	return (0);
+}
 
 int main(void)
 {
-	void	*mlx;
-	void	*mlx_window;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, 1920, 1080, "Typical window");
-	mlx_loop(mlx);
+	data.mlx = mlx_init();
+	data.mlx_win= mlx_new_window(data.mlx, 1000, 1000, "Typical window");
+	mlx_key_hook(data.mlx_win, &handle_key, &data);
+
+	mlx_loop(data.mlx);
+	return (0);
 }
