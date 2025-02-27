b7c027ce6bdd535888ea73953c1e796f84fafdcb
sboukiou <sboukiou@student.1337.ma>
2025-02-27 20:01:29 +0000

    Added mlx_loop_hook to insert drawing functions

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "fdf.h"
 
+/**
+ * terminate - Ends the program with the corresponding message
+ * @code: Error code to display
+ */
 void	terminate(char *code)
 {
 	write(STDOUT_FILENO, "Faile to establish mlx connection:", 35);
@@ -13,15 +17,14 @@ void	terminate(char *code)
 	* @key_code: The number corresponding to the key
 	* @data: Info about the mlx and mlx_window ptrs
 	* @Return: 0 , Or quit berfore if it's ESC
-*/
-
-int	handle_key(int key_code, t_data *data)
+	*/
+int	handle_key(int key_code, t_mlx_session *mlx_session)
 {
 	if (key_code == ESCAPE)
 	{
-		mlx_destroy_window(data->mlx, data->mlx_win);
-		mlx_destroy_display(data->mlx);
-		free(data->mlx);
+		mlx_destroy_window(mlx_session->mlx, mlx_session->mlx_win);
+		mlx_destroy_display(mlx_session->mlx);
+		free(mlx_session->mlx);
 		exit(0);
 	}
 	return (0);
@@ -32,24 +35,24 @@ int	handle_key(int key_code, t_data *data)
 	* @ac: Number of given arguments
 	* @av: given args as array of strings
 	* @Return: 0 on sucess
-*/
-
+	*/
 int main(void)
 {
-	t_data	data;
+	t_mlx_session	mlx_session;
 
 	/*Initializing the mlx connection to the X server*/
-	data.mlx = mlx_init();
-	if (!data.mlx)
+	mlx_session.mlx = mlx_init();
+	if (!mlx_session.mlx)
 		terminate(MLX_INIT);
 	/*Creating the window with size + title*/
-	data.mlx_win= mlx_new_window(data.mlx, 1000, 1000, "Typical window");
-	if (!data.mlx_win)
+	mlx_session.mlx_win= mlx_new_window(mlx_session.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Typical window");
+	if (!mlx_session.mlx_win)
 		terminate(MLX_WINDOW);
 	/*Binding the keyPress with the handle_key function*/
-	mlx_hook(data.mlx_win, KEY_PRESS_EVENT, KEY_PRESS_MASK, handle_key, &data);
-	mlx_pixel_put(&data.mlx, &data.mlx_win, 5, 5, 0xFF0000);
+	mlx_hook(mlx_session.mlx_win, KEY_PRESS_EVENT, KEY_PRESS_MASK, handle_key, &mlx_session);
+	/*mlx_pixel_put(&mlx_session.mlx, &mlx_session.mlx_win, 5, 5, 0xFF0000);*/
+	mlx_loop_hook(mlx_session.mlx, draw_circle, &mlx_session);
 
-	mlx_loop(data.mlx);
+	mlx_loop(mlx_session.mlx);
 	return (0);
 }
