a855003e42fff2e492c537ccf1bb68b08ae18806
sboukiou <sboukiou@student.1337.ma>
2025-02-28 01:29:05 +0000

    Some norme adjusting

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboukiou <sboukiou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/27 20:03:30 by sboukiou          #+#    #+#             */
+/*   Updated: 2025/02/27 20:32:06 by sboukiou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
 /**
@@ -36,7 +48,7 @@ int	handle_key(int key_code, t_mlx_session *mlx_session)
 	* @av: given args as array of strings
 	* @Return: 0 on sucess
 	*/
-int main(void)
+int	main(void)
 {
 	t_mlx_session	mlx_session;
 
@@ -45,13 +57,14 @@ int main(void)
 	if (!mlx_session.mlx)
 		terminate(MLX_INIT);
 	/*Creating the window with size + title*/
-	mlx_session.mlx_win= mlx_new_window(mlx_session.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Typical window");
+	mlx_session.mlx_win= mlx_new_window(mlx_session.mlx,
+						WINDOW_WIDTH, WINDOW_HEIGHT, "Typical window");
 	if (!mlx_session.mlx_win)
 		terminate(MLX_WINDOW);
 	/*Binding the keyPress with the handle_key function*/
 	mlx_hook(mlx_session.mlx_win, KEY_PRESS_EVENT, KEY_PRESS_MASK, handle_key, &mlx_session);
 	/*mlx_pixel_put(&mlx_session.mlx, &mlx_session.mlx_win, 5, 5, 0xFF0000);*/
-	mlx_loop_hook(mlx_session.mlx, draw_circle, &mlx_session);
+	mlx_loop_hook(mlx_session.mlx, draw_shapes, &mlx_session);
 
 	mlx_loop(mlx_session.mlx);
 	return (0);
