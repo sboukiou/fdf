6f578dd9e11335c842aec5229b8c22d004e8c587
sboukiou <sboukiou@student.1337.ma>
2025-02-28 02:03:25 +0000

    Addin ft_memecpy source and proto

--- dependencies/libft/libft.h
+++ dependencies/libft/libft.h
@@ -26,6 +26,7 @@ typedef struct s_list
 
 /*Functions prototypes*/
 int		ft_isdigit(int c);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
