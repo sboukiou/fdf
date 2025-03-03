e3b3bb121b06ecf939091cb8e7205ee69557febe
sboukiou <sboukiou@student.1337.ma>
2025-03-03 04:40:14 +0000

    Addding some drawing functions to be used for testing

--- drawer.c
+++ drawer.c
@@ -12,7 +12,6 @@
 
 #include "./fdf.h"
 
-
 /**
  * draw_square - Draws a square with a corresponding sides
  * @session: Mlx session to draw in
@@ -42,19 +41,32 @@ int	draw_circle(t_mlx_session *session)
 {
 	if (!session || !session->img)
 		return (FAIL);
-	for (int i = 0; i < WINDOW_HEIGHT; i++)
+	for (int i = 0; i < 300; i ++)
 	{
-		for(int j = 0; j < WINDOW_WIDTH; j++)
-		{
-			if (abs(distance(j, i, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2) - 9000) < 200)
-				mlx_put_to_image(session->img, j, i, GREEN);
-		}
+		for (int j = 0; j < 300; j++)
+			if (j == 0 || j == 299 || i == 0 || i == 299)
+				mlx_put_to_image(session->img, j + SCALE, i + SCALE, GREEN);
 	}
 	return (SUCCESS);
 }
 
-int	draw_line(t_mlx_session *session)
+int draw_line(t_mlx_session *session, int x1, int y1, int x2, int y2, int color)
 {
-	
+	t_pt	a, b, cords;
+	a.x =  x1;
+	b.x =  x2;
+	a.y =  y1;
+	b.y =  y2;
+	for (int i = ft_min(y1, y2); i < ft_max(y1, y2); i++)
+	{
+		for (int j = ft_min(x1, x2); j < ft_max(x1, x2); j++)
+		{
+			cords.x = j;
+			cords.y = i;
+			if (triangle_area(a, b, cords) == 0)
+				mlx_put_to_image(session->img, j, i, color);
+		}
+	}
+	return (0);
 }
 
